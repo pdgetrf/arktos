32fb693179d3bbd781e37094bd5f442f8998a4a7
Peng Du <pdu@futurewei.com>
2021-02-23 20:54:23 +0000

    fix

--- hack/lib/util.sh
+++ hack/lib/util.sh
@@ -508,14 +508,6 @@ function kube::util::write_client_kubeconfig {
     local client_id=$6
     local token=${7:-}
 
-    IS_RESOURCE_PARTITION=${IS_RESOURCE_PARTITION:-}
-
-    if [[ -z "${IS_RESOURCE_PARTITION}" ]]; then
-       MASTER_ENDPOINT="https://${API_HOST}:${API_SECURE_PORT}"
-    else
-       MASTER_ENDPOINT=${SCALE_OUT_PROXY_ENDPOINT}
-    fi
-
     cat <<EOF | ${sudo} tee "${dest_dir}"/"${client_id}".kubeconfig > /dev/null
 apiVersion: v1
 kind: Config
